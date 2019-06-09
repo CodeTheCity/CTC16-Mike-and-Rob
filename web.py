3a1ad1be5fee16e08ceee1bb31586620c09dbdbf
Robert McWilliam <ormiret@ormiret.com>
2019-06-09 12:20:00 +0000

    Get web stuff to actually running.

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request, json, redirect
+import colorsys
+from random import random
 
 app = Flask(__name__)
 
@@ -6,8 +8,11 @@ def randc():
     r,g,b = colorsys.hsv_to_rgb(random(), 1, 1.0)
     return [int(r*1024), int(g*1024), int(b*1024)]
 
+@app.route("/")
+def root():
+    return jsonify({"msg":"OK"})
 
 @app.route("/test")
 def test():
     c = randc()
-    return jsonify({"r":c[0], "g":c[1], "b":c[1], "w":0}) 
+    return jsonify({"r":c[0], "g":c[1], "b":c[2], "w":0}) 
